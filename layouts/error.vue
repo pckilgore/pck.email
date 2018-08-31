68b7556e747a775149207ef1ea2c1a361d34b7d0
Patrick Kilgore <pckilgore@noreply.github.com>
2018-08-30 21:50:57 -0500

    main pages WIP

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-text>Error</v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
