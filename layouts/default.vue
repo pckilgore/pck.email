503962e56f466ca6f9acf5a4f8a8ede31a76f298
Patrick Kilgore <pckilgore@noreply.github.com>
2018-08-30 21:50:57 -0500

    main pages WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,20 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+  <v-app>
+    <v-toolbar fixed app dark flat>
       <v-spacer></v-spacer>
       <v-btn
-        icon
+        right
+        light
+      >
+        <!-- Trigger Modal contact stuff -->
+        POST :://
+      </v-btn>
+      <v-btn
+        right
+        light
         @click.stop="rightDrawer = !rightDrawer"
       >
-        <v-icon>menu</v-icon>
+        GET :://
       </v-btn>
     </v-toolbar>
     <v-content>
@@ -60,21 +24,21 @@
     </v-content>
     <v-navigation-drawer
       temporary
-      :right="right"
+      right
       v-model="rightDrawer"
       fixed
+      color="black"
     >
       <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        <v-list-tile v-for="page in pages" :key="page.id">
+          <v-list-tile-title>{{page.name}}</v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+      <div class="pl-3">PATCH :// this page @ <a href="http://github.com/pckilgore/pck.email">github</a> </div>
+      <v-spacer></v-spacer>
+      <span>&copy; 2017 Patrick C. Kilgore</span>
     </v-footer>
   </v-app>
 </template>
@@ -83,9 +47,9 @@
   export default {
     data () {
       return {
-        clipped: false,
         drawer: true,
         fixed: false,
+        pages: [{id:1, name:"Resume"}],
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
           { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
